3e571fc72f58229863e80d40c8ff3b164c8dbda9
Daniel Krupiński <danielkrupinski@outlook.com>
2018-01-31 17:42:22 +0100

    Add results output

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <cstdlib>
 
 using namespace std;
 
@@ -9,8 +8,14 @@ int main()
 	cout << "Enter amount of flips: ";
 	int amount {0};
 	cin >> amount;
+	srand(time(NULL));
+	system("clear");
+	int amount_1 {0};
 	for (int i=0; i!=amount; ++i)
-		rand() % 2 == 0 ? cout << 1 << endl : cout << 2 << endl;
+		if (rand() % 2 == 0)
+			++amount_1;
+	cout << "Results:\n\v";
+	cout << amount_1 << " heads and " << amount-amount_1 << " tails of total " << amount << " flips.";
 	cin.ignore();
 	getchar();
 	return 0;
