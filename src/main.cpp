5d592c20b59676404fb2c97dfe3085311dd165d5
Daniel Krupiński <danielkrupinski@outlook.com>
2018-02-01 11:57:53 +0100

    Clean main() function

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,11 @@
-#include <iostream>
+#include "../include/functions.h"
 
 using namespace std;
 
 int main()
 {
-	cout << "Coinflip simulator\n\v";
-	cout << "Enter amount of flips: ";
-	int amount {0};
-	cin >> amount;
-	srand(time(NULL));
-	system("clear");
-	int amount_1 {0};
-	for (int i=0; i!=amount; ++i)
-		if (rand() % 2 == 0)
-			++amount_1;
-	cout << "Results:\n\v";
-	cout << amount_1 << " heads and " << amount-amount_1 << " tails of total " << amount << " flips.";
-	cin.ignore();
-	getchar();
+	data Data = setup();
+	generate(Data);
+	results(Data);
 	return 0;
 }
