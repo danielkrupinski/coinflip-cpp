55bfd1af1a3d40ca85e61d3e35b038388e1c3f94
Daniel Krupiński <danielkrupinski@outlook.com>
2018-02-03 16:49:49 +0100

    Update data struct

--- include/functions.h
+++ include/functions.h
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
 
-struct data {int amount {0}, mode {0}, heads {0}; std::vector<int> random;};
+struct data {int amount {}, mode {}, heads {}; std::vector<int> random {};};
 
 data setup();
 
