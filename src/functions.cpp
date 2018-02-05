8fcd54b42c84d71750a9275ca23a8fc32ca81fca
Daniel Krupiński <danielkrupinski@outlook.com>
2018-02-05 01:18:28 +0100

    Add additional info message

--- src/functions.cpp
+++ src/functions.cpp
@@ -28,6 +28,7 @@ void results(data& Data)
 	remove("results.txt");
 	ofstream file;
 	file.open("results.txt");
+	file << Data.heads << " heads and " << Data.amount-Data.heads << " tails";
 	file << "0 - heads, 1 - tails\n\n";
 	for (int i=0; i!=Data.random.size(); ++i)
 		file << Data.random[i] << endl;
