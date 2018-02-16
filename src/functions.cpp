71dc440fea6abfbcd5408187858c6e3841fa423a
Daniel Krupiński <danielkrupinski@outlook.com>
2018-02-16 18:56:16 +0100

    Update code formatting

--- src/functions.cpp
+++ src/functions.cpp
@@ -17,7 +17,7 @@ void generate(data& Data)
     for (int i=0; i!=Data.amount; ++i)
     {
         if (int random {rand()%2})
-        Data.random.push_back(1);
+            Data.random.push_back(1);
         else
         {
             Data.random.push_back(0);
@@ -35,7 +35,7 @@ void results(const data& Data)
     file << Data.heads << " heads and " << Data.amount-Data.heads << " tails of total " << Data.amount << " flips.\n";
     file << "0 - heads, 1 - tails\n\n";
     for (auto& x : Data.random)
-    file << x << ',';
+        file << x << ',';
     file.close();
     cout << "Results have been saved in results.txt file.\n";
 }
